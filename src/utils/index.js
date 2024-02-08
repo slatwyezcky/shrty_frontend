34d3b5e2791100469106ffb666adf432c694fb0c
Pavel Slatvitskii <pavel.slatvitskiy@gmail.com>
2024-02-08 18:59:31 +0600

    Fix util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ export function checkURL(url) {
   if (!url) return false;
   const regexp = new RegExp(
     // prettier-ignore
-    /((https:\/\/)|(http:\/\/))?(www\.)?([a-zA-Z0-9_]{1,}\.){1,2}[a-zA-Z]{1,}(\/[a-zA-Z0-9#$%&_?=]{1,})*(\.[a-zA-Z]{1,})?/
+    /((https:\/\/)|(http:\/\/))?(www\.)?([a-zA-Z0-9_]{1,}\.){1,2}[a-zA-Z]{1,}(\/[a-zA-Z0-9#$%&_?=]{1,})*(\.[a-zA-Z]{1,})?([a-zA-Z0-9#$%&_?=]{1,})*/
   );
   return regexp.test(url);
 }
